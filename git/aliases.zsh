139c654f7da78a4a6e8d3ad4a238ba8bb153b9cc
Tom Bernaerd <tom@bernaerd.be>
2015-11-02 22:17:14 +0100

    change some aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -10,13 +10,13 @@ fi
 alias g='git'
 
 # The rest of my fun git aliases
+alias gs="git status"
 alias gp='git pull --prune'
 alias gpu='git push origin HEAD'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gd='git diff'
-alias gc='git commit'
-alias gca='git commit -a'
-alias gch='git checkout'
+alias gcomm='git commit'
+alias gc='git checkout'
 alias gb='git branch'
 alias ga='git add'
 alias gap='git add -p'
